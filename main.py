6a6b93dff7bfe50dc336bd3829b7d66658cb8330
Igor Konovalenko <skaylic@yandex.ru>
2024-10-29 08:56:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+from time import sleep
+
 from dotenv import load_dotenv
 from okx.exceptions import OkxAPIException
 from skay.Logger import setup_logger
 from skay.Bot import Bot
+from httpx import ReadTimeout, ConnectTimeout
 
 load_dotenv()
 logger = setup_logger()
 
-bot = Bot()
-
 
 def run():
     try:
+        bot = Bot()
         bot.start()
     except KeyboardInterrupt:
         logger.info('Keyboard interrupt received, shutting down...')
     except OkxAPIException as e:
         logger.error(str(e))
-    except Exception as e:
+    except ConnectTimeout as e:
         logger.error(str(e))
+        sleep(20)
+        run()
+    except ReadTimeout as e:
+        logger.error(str(e))
+        sleep(20)
+        run()
 
 
 if __name__ == '__main__':
